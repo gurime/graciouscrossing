f621c8ad161e5af3a8cbf170e49d20fafd1ac8c4
Phillip Bailey <ztron500@gmail.com>
2023-02-15 15:56:24 -0500

    Update Advertise.jsx

--- src/Pages/Advertise.jsx
+++ src/Pages/Advertise.jsx
@@ -6,7 +6,37 @@ export const Advertise = () => {
 return (
 <>
 <Navbar/>
-Advertise
+<div style={{
+margin:'30px auto',
+borderBottom:'solid blue 1px'
+}}>
+<h1 style={{
+textAlign:'center',
+fontSize:'30px',
+marginBottom:'30px'
+}}>Advertise Your Business with Gracious Crossing
+</h1>
+<p style={{
+fontSize:'17px',
+textAlign:'center',
+marginBottom:'30px'
+}}>Reach millions of buyers, dealers and leaseholders on the biggest genuine network organization on the net.
+
+</p>
+</div>
+
+<div style={{
+textAlign:'center'
+}}>
+<h1 style={{
+fontWeight:'100',
+textTransform:'capitalize'
+}}
+>Select your industry</h1>
+</div>
+
+
+
 <Footer/>
 </>
 )
